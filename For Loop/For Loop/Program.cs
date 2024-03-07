a9a4970d7b3b02b01e7e6bfea385a81d18a43e2b
aamritbistaa <aamritbistaa@gmail.com>
2024-03-08 00:37:13 +0545

    for loop

---
+++ For Loop/For Loop/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Specialized;
+
+for (int i=0; i < 5; i++)
+{
+    Console.WriteLine($"Hello{i}");
+}
+string data = "Ramesh,is,good,but,shyam,is,not";
+
+var dataInArray = data.Split(',').ToList();
+
+for(int i = 0; i < dataInArray.Count; i++)
+{
+    Console.WriteLine(dataInArray[i]);
+}
+
+List<decimal> charges = new();
+charges.Add(23.14M);
+charges.Add(4M);
+charges.Add(23M);
+
+decimal Sum = 0;
+for(int i=0; i<charges.Count; i++)
+{
+    Sum = Sum + charges[i]; 
+}
+
+Console.WriteLine(Sum);
