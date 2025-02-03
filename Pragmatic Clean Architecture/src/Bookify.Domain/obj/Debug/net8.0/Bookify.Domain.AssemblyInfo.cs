7e4c5bcdf5fe9883967e24adcc60aa13543e6042
aamritbistaa <aamritbistaa@gmail.com>
2025-02-03 20:37:49 +0545

    added configuration for booking entity and also injected configurations into dbcontext

--- Pragmatic Clean Architecture/src/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
+++ Pragmatic Clean Architecture/src/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fc53ee6a914942d4499c42a93b30f20d8efbb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711bf09b154fe1492fd465736887fdd62bbb1fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
