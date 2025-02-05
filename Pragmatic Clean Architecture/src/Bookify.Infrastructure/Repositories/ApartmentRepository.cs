b875a4744ef6fae05cc271944f9925c56613b444
amrit.technofex <amrit.bista@np.technofex.com>
2025-02-05 16:34:11 +0545

    added repository

---
+++ Pragmatic Clean Architecture/src/Bookify.Infrastructure/Repositories/ApartmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Apartments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repositories
+{
+    internal sealed class ApartmentRepository : Repository<Apartment>, IApartmentRepository
+    {
+        public ApartmentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await base.GetByIdAsync(id);
+        }
+    }
+}
---
+++ Pragmatic Clean Architecture/src/Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bookify.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repositories
+{
+    internal sealed class UserRepository : Repository<User>,IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await base.GetByIdAsync(id);
+        }
+
+        public async Task Add(User user)
+        {
+           await base.Add(user);
+        }
+    }
+}
