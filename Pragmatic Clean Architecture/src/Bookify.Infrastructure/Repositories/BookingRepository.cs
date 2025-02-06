6f2a7ba1cea96ed54842eed7454f7c970a572bbc
amrit.technofex <amrit.bista@np.technofex.com>
2025-02-06 17:52:28 +0545

    booking repo

---
+++ Pragmatic Clean Architecture/src/Bookify.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,32 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Repositories
+{
+    internal class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        public BookingRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IBookingRepository.Add(Booking booking)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Booking> IBookingRepository.GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
