14d7f618e4e1873c64b99d7a7440fec30732e173
aamritbistaa <amrit.bista@np.technofex.com>
2025-02-14 17:32:25 +0545

    added apartment controller

---
+++ Pragmatic Clean Architecture/src/Bookify.Api/Controllers/ApartmentsController.cs
@@ -0,0 +1,28 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ApartmentsController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchApartments(DateOnly startDate, DateOnly endDate)
+        {
+
+            var query = new SearchApartmentQuery (startDate,endDate);
+            var result = await _sender.Send(query);
+            return Ok(result.Value);
+        }
+    }
+}
