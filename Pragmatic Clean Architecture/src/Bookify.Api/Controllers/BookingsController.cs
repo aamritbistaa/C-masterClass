cc436845582391c8344b4c5ec95c67fde200b625
aamritbistaa <amrit.bista@np.technofex.com>
2025-02-14 17:42:37 +0545

    test

---
+++ Pragmatic Clean Architecture/src/Bookify.Api/Controllers/BookingsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+    }
+}
--- Pragmatic Clean Architecture/src/Bookify.Api/obj/Debug/net8.0/Bookify.Api.AssemblyInfo.cs
+++ Pragmatic Clean Architecture/src/Bookify.Api/obj/Debug/net8.0/Bookify.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b2d9ec12276630f9d178f4585503112b8374de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d7f618e4e1873c64b99d7a7440fec30732e173")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
