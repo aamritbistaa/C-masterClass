34c7e5d9ebdf7aff6cd63d718da335e5301beec2
aamritbistaa <aamritbistaa@gmail.com>
2025-06-30 23:43:42 +0545

    renived ybbecessary files

--- Udemy-Ecommerce/Services/Basket/Basket.Api/obj/Debug/net8.0/Basket.Api.AssemblyInfo.cs
+++ Udemy-Ecommerce/Services/Basket/Basket.Api/obj/Debug/net8.0/Basket.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basket.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1ecf8cfb158d242310ddea03637ac2d6c01b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30ba3f390ff1c2d6e8e37bde3ee412404c0535d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basket.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basket.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
